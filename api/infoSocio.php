9daf7d20a84e4fc69edc29cdda216c1d84dabf99
Kevin97Palma <67132452+Kevin97Palma@users.noreply.github.com>
2023-11-10 16:44:26 -0500

    Modificacion api ahora llama con cedula

--- api/infoSocio.php
+++ api/infoSocio.php
@@ -9,12 +9,26 @@ try {
     // Crear instancia de PDO usando tu configuración
     $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8", $dbUser, $dbPass, $options);
 
-    // Realizar la consulta
-    $sql = "SELECT ID_Socio, Nombre, Apellido, CorreoElectronico, NumeroTelefono, Direccion, FechaInsercion, FechaActualizacion, ID_Colegio FROM fnc.socios";
-    $result = $pdo->query($sql);
+    // Obtener el ID_Socio de la solicitud GET
+    $idSocio = isset($_GET['ci']) ? $_GET['ci'] : null;
+
+    if ($idSocio !== null) {
+        // Si se proporciona un ID_Socio, construir y ejecutar la consulta con WHERE
+        $sql = "SELECT ID_Socio,Cedula, Nombre, Apellido, CorreoElectronico, NumeroTelefono, Direccion, FechaInsercion, FechaActualizacion, ID_Colegio FROM fnc.socios WHERE Cedula = :ci";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':ci', $idSocio, PDO::PARAM_INT);
+        $stmt->execute();
+    } else {
+        // Si no se proporciona un ID_Socio, realizar la consulta sin WHERE
+        $sql = "SELECT ID_Socio,Cedula, Nombre, Apellido, CorreoElectronico, NumeroTelefono, Direccion, FechaInsercion, FechaActualizacion, ID_Colegio FROM fnc.socios";
+        $result = $pdo->query($sql);
+        $data = $result->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($data);
+        exit();  // Terminar la ejecución después de devolver la respuesta sin WHERE
+    }
 
     // Obtener los resultados como un array asociativo
-    $data = $result->fetchAll(PDO::FETCH_ASSOC);
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     // Devolver la respuesta como JSON
     echo json_encode($data);
